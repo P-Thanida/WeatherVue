488a73c06e5748f68ffde548e0814028d73c4f8a
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-20 16:23:13 +0700

    Add Event

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,15 @@
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{weather.name}} , {{weather.sys.country}}</div>
+          <div class="location">
+            {{ weather.name }} , {{ weather.sys.country }}
+          </div>
           <div class="date">Friday 20 May 2022</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9 °C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -41,18 +43,52 @@ export default {
   },
   methods: {
     fetchWeather(e) {
-      if(e.key == "Enter"){
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(res => {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
             return res.json();
-          }).then(this.setResults);
+          })
+          .then(this.setResults);
       }
     },
-    setResults (results) {
+    setResults(results) {
       this.weather = results;
-    }
-  }
-}
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
 </script>
 
 <style>
