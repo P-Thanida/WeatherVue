ca8c18657f4f79b4bca7eb431360988695f8df71
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-20 16:16:13 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,27 @@
   <div class="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+        <!-- {{ query }} -->
         <!-- <div class="test" id="yes"></div> -->
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
+          <div class="location">{{weather.name}} , {{weather.sys.country}}</div>
           <div class="date">Friday 20 May 2022</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9 °C</div>  
+          <div class="temp">9 °C</div>
           <div class="weather">Rain</div>
         </div>
-
-        
       </div>
     </main>
   </div>
@@ -25,46 +30,65 @@
 
 <script>
 export default {
-  name: 'App',
-  data(){
-    return{
-      api_key: 'ff3f95f18d2052d7b13f83f3714ecd45'
+  name: "App",
+  data() {
+    return {
+      api_key: "ff3f95f18d2052d7b13f83f3714ecd45",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
     }
   }
 }
-
 </script>
 
 <style>
- *{
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
- }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
- body{
-   font-family: 'montserrat', sans-serif;
- }
+body {
+  font-family: "montserrat", sans-serif;
+}
 
-#app{
-  background-image: url('./assets/cold-bg.jpg');
+#app {
+  background-image: url("./assets/cold-bg.jpg");
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 
-main{
+main {
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25),rgba(0,0,0,0.75) );
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
 }
 
-.search-box{
+.search-box {
   width: 100%;
   margin-bottom: 30px;
 }
 
-.search-box .search-bar{
+.search-box .search-bar {
   display: block;
   width: 100%;
   padding: 15px;
@@ -74,15 +98,15 @@ main{
   border: none;
   outline: none;
   background: none;
-  box-shadow: 0px 0px 8px rgba(255,255,255,0.25);
-  background-color: rgba(255,255,255,0.5);
+  box-shadow: 0px 0px 8px rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
 
-.search-box .search-bar:focus{
+.search-box .search-bar:focus {
   background-color: rgba(255, 255, 255, 0.75);
-  box-shadow: 0px 0px 16px rgba(255,255,255,0.25);
+  box-shadow: 0px 0px 16px rgba(255, 255, 255, 0.25);
   border-radius: 16px 0px 16px 0px;
 }
 
@@ -113,18 +137,17 @@ main{
   font-size: 102px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255,255,255,0.25);
+  background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
   box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
-.weather-box .weather{
+.weather-box .weather {
   color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
-
 </style>
